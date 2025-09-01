b93daaacb7fb75b816c864ffce9354e03bd9c0c8
Muqoddim-arotayo <112625006+Muqoddim-arotayo@users.noreply.github.com>
2025-09-01 11:09:10 +0100

    added a tkinter class file

---
+++ tKinter.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("420x420")
+window.title("Tkinter class")
+icon = PhotoImage(file='Py_class/download.png')
+window.iconphoto(True,icon)
+window.config(background="cyan")
+label = Label()
+window.mainloop()
